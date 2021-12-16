300e1d3084dd167bd6965bd06b8ea8e02237fe08
Jesse Graf <jesse@grafip.com>
2021-12-16 16:09:50 +1000

    Added a rough soundView and a variable for the tonic note to be chosen from

--- ScaleHero/Views/ArpeggioView.swift
+++ ScaleHero/Views/ArpeggioView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ArpeggioView : View {
     
     let universalSize = UIScreen.main.bounds
+    @State var noteName = "c"
     
     @Binding var screenType: String
     
@@ -26,8 +27,13 @@ struct ArpeggioView : View {
                     Spacer()
                     
                     // Turn to image button
-                    Button("Major (Triad)") {
-                        // do nothing
+                    Button("Select Note") {
+                        noteName = "d"
+                    }.padding()
+                    
+                    // Turn to image button
+                    NavigationLink(destination: SoundView(scaleType: noteName +  " Major arpeggio")) {
+                        Text("Major (Triad)")
                     }.padding()
                     
                     // Turn to image button
@@ -37,6 +43,11 @@ struct ArpeggioView : View {
                     
                     // Turn to image button
                     Button("Special (Tetrads)") {
+                        // do nothing
+                    }.padding()
+                    
+                    // Turn to image button
+                    Button("Settings") {
                         // do nothing
                     }.padding()
                     
