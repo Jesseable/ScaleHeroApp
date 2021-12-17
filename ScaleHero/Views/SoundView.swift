050fba42dc3846662fda2c152a4c9747b2de4c56
Jesse Graf <jesse@grafip.com>
2021-12-17 19:47:33 +1000

    Added a drop down menu for metronome and octave selection. Also created a checkbox for the drone

--- ScaleHero/Views/SoundView.swift
+++ ScaleHero/Views/SoundView.swift
@@ -13,6 +13,10 @@ struct SoundView : View {
     let universalSize = UIScreen.main.bounds
     
     @State var scaleType: String
+    @State var isPlaying = false
+    @State var numOctave = 1
+    @State var tempo = 100
+    @State var drone = true
     
     var body: some View {
         
@@ -21,11 +25,56 @@ struct SoundView : View {
         
             VStack {
                 Text(scaleType).bold().textCase(.uppercase).font(.title).foregroundColor(.white).padding()
+                
                 Spacer()
-                // Turn to image button
-                Button("Play") {
-                    // do nothing
+                
+                Button {
+                    if (isPlaying) {
+                        Sound.stopAll()
+                        isPlaying = false
+                    } else {
+                        Sound.play(file: "small-loop.mp3", numberOfLoops: 1) // Test with a proper sound file
+                        isPlaying = true
+                    }
+                } label: {
+                    if isPlaying {
+                        Text("Stop")
+                    } else {
+                        Text("Play")
+                    }
                 }.padding()
+
+                // Ovtaves view
+                Menu("Number of Octaves = " + String(numOctave)) {
+                    Button("1 octave", action: {numOctave = 1})
+                    Button("2 octaves", action: {numOctave = 2})
+                    Button("3 octaves", action: {numOctave = 3})
+                }
+            
+                Menu("Tempo = " + String(tempo)) {
+                    ForEach(40..<221) { i in
+                        if (i % 10 == 0) {
+                            Button("Tempo: " + String(i), action: {tempo = i})
+                        }
+                    }
+                }
+                
+                HStack {
+                    Spacer()
+                    }
+                    if (drone) {
+                        Image(systemName: "square").foregroundColor(Color.white)
+                    } else {
+                        Image(systemName: "checkmark.square").foregroundColor(Color.white)
+                    }
+                    Button("Drone") {
+                    drone.toggle()
+                    Spacer()
+                }
+                
+                
+                Spacer()
+                Spacer()
             }
         }
     }
