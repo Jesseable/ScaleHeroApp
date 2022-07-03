8e811fc35ab95ae477d381c05ae96bcaefdb2bf4
Jesse Graf <jesse@grafip.com>
2022-07-03 12:37:13 +1000

    Starts adding images in a circular fashion preparing for the circle of fifths display

--- ScaleHero/Views/NoteSelectionView.swift
+++ ScaleHero/Views/NoteSelectionView.swift
@@ -21,7 +21,7 @@ struct NoteSelectionView: View {
 
     var body: some View {
         
-        let titleImage = Image("ScaleHero" + fileReaderAndWriter.readBackgroundImage())
+        let titleImage = Image("ScaleHeroBlue") //+ fileReaderAndWriter.readBackgroundImage())
         let buttonHeight = universalSize.height/10
         
         ZStack {
@@ -59,6 +59,11 @@ struct NoteSelectionView: View {
                     .frame(height: UIScreen.main.bounds.height/6)
                     .padding()
                 
+                ZStack {
+                    CircleOfFifthButtons(radius: universalSize.width * 0.4)
+                    Circle().opacity(0.5)
+                    CircleOfFifthButtons(radius: universalSize.width * 0.25)
+                }
                
                 Spacer()
                     
@@ -78,58 +83,112 @@ struct NoteSelectionView: View {
 struct CircleOfFifthButtons: View {
     
     private let universalSize = UIScreen.main.bounds
+    let colour = "BlueDark"
+    let count = 12
+    let radius : CGFloat
     
     var body: some View {
-        HStack {
-            NoteSelectionButtons()
-                .fill(style: FillStyle(eoFill: true))
-                .foregroundColor(Color("LightGrayBasic"))
-                .padding(.horizontal, 10)
-                .frame(width: universalSize.width * 0.18, height: universalSize.height * 0.08)
-                .opacity(0.8)
-                .rotationEffect(Angle(degrees: -30))
-                .padding(.top, 40)
+        let currentAngle: CGFloat = 0
+        let buttonAngle: CGFloat = (CGFloat(360 / count)) * .pi / 180
+        let centre = CGPoint(x: universalSize.midX, y: universalSize.maxY * 0.3)
+        //let radius = universalSize.width * 0.4
+        
+        ZStack {
+            Group {
+                let firstPos = placePos(around: centre, radius: radius,
+                                        currentAngle: currentAngle)
+                NoteSelectionButtons(colour: colour) /// MAKE IT A BUTTON WITH TEXT
+                    .position(x: firstPos.x, y: firstPos.y)
+                
+                let secondPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 1))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: secondPos.x, y: secondPos.y)
+                
+                let thirdPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 2))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: thirdPos.x, y: thirdPos.y)
+                
+                let fourthPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 3))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: fourthPos.x, y: fourthPos.y)
+            }
             
-            NoteSelectionButtons()
-                .fill(style: FillStyle(eoFill: true))
-                .foregroundColor(Color("LightGrayBasic"))
-                .padding(.horizontal, 10)
-                .frame(width: universalSize.width * 0.18, height: universalSize.height * 0.08)
-                .opacity(0.8)
-            
-            NoteSelectionButtons()
-                .fill(style: FillStyle(eoFill: true))
-                .foregroundColor(Color("LightGrayBasic"))
-                .padding(.horizontal, 10)
-                .frame(width: universalSize.width * 0.18, height: universalSize.height * 0.08)
-                .opacity(0.8)
-                .rotationEffect(Angle(degrees: 30))
-                .padding(.top, 40)
+            Group {
+                let fifthPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 4))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: fifthPos.x, y: fifthPos.y)
+                
+                let sixthPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 5))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: sixthPos.x, y: sixthPos.y)
+                
+                let seventhPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 6))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: seventhPos.x, y: seventhPos.y)
+                
+                let eighthPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 7))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: eighthPos.x, y: eighthPos.y)
+            }
+                
+            Group {
+                let ninethPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 8))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: ninethPos.x, y: ninethPos.y)
+                
+                let tenthPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 9))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: tenthPos.x, y: tenthPos.y)
+                
+                let eleventhPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 10))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: eleventhPos.x, y: eleventhPos.y)
+                
+                let twelvethPos = placePos(around: centre, radius: radius,
+                                         currentAngle: (currentAngle + buttonAngle * 11))
+                NoteSelectionButtons(colour: colour)
+                    .position(x: twelvethPos.x, y: twelvethPos.y)
+            }
         }
     }
-}
-
-struct NoteSelectionButtons: Shape {
     
-    func path(in rect: CGRect) -> Path {
-        
-        let path = UIBezierPath(rect: rect)
-        // No need to move
-        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY),
-                          controlPoint: CGPoint(x: rect.midX, y: rect.maxY * 0.9))
-        // At top right
-        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-        //path.addLine(to: CGPoint(x: rect.maxX * (1.1), y: rect.minY))
-        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.minY),
-                          controlPoint: CGPoint(x: rect.midX, y: 0 - rect.maxY * 0.1))
-        return Path(path.cgPath)
+    @ViewBuilder func NoteSelectionButtons(colour: String) -> some View {
+        Circle()
+            .fill(Color(colour))
+            .frame(height: universalSize.width * 0.12, alignment: .center)
+    }
+    
+    private func placePos(around center: CGPoint, radius: CGFloat,
+                       currentAngle: CGFloat) -> (x: CGFloat, y: CGFloat) {
+
+        let xPos = center.x + cos(currentAngle) * radius
+        let yPos = center.y + sin(currentAngle) * radius
+//        let buttonCenter = CGPoint(x: center.x + cos(currentAngle) * radius, y: center.y + sin(currentAngle) * radius)
+
+        // (1)
+        //button.transform = CGAffineTransform(rotationAngle: currentAngle)
+
+        // (2)
+        //view.addSubview(button)
+        return (xPos, yPos)
+//        button.center = buttonCenter
+//        currentAngle += buttonAngle
     }
 }
 
 struct NoteSelectionView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleOfFifthButtons()
-        //NoteSelectionView(backgroundImage: "BackgroundBlack")
+        //CircleOfFifthButtons()
+        NoteSelectionView(backgroundImage: "BackgroundBlue")
     }
 }
